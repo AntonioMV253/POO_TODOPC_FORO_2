a05a313e394550ddf8a3eb8191361f72971b202d
Antonio <miltonmayorgavasquez@gmail.com>
2025-09-18 20:27:17 -0600

    Fix constructor only in Tablet.java

--- todopc/src/Equipo.java
+++ todopc/src/Equipo.java
@@ -3,12 +3,4 @@ public abstract class Equipo {
     public String modelo;
     public String microprocesador;
     public String memoria;
-
-    public Equipo(String fabricante, String modelo, String microprocesador,String memoria){
-        this.fabricante = fabricante; 
-        this.modelo = modelo;
-        this.microprocesador =microprocesador; 
-        this.memoria = memoria;
-    }
-
 }
--- todopc/src/Tablet.java
+++ todopc/src/Tablet.java
@@ -4,14 +4,16 @@ public class Tablet extends Equipo {
     public String tamanoMemoriaNAND;
     public SistemaOperativo sistemaOperativo;
 
-    public Tablet(String fabricante, String modelo, String microproccesador, String memoria, String tamanoDiagonal, TipoPantalla tipoPantalla, String tamanoMemoriaNAND, SistemaOperativo sistemaOperativo){
-
-        super(fabricante, modelo, microproccesador, memoria);
+    public Tablet(String fabricante, String modelo, String microprocesador, String memoria,
+                  String tamanoDiagonal, TipoPantalla tipoPantalla,
+                  String tamanoMemoriaNAND, SistemaOperativo sistemaOperativo) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.microprocesador = microprocesador;
+        this.memoria = memoria;
         this.tamanoDiagonal = tamanoDiagonal;
-        this.tipoPantalla =tipoPantalla;
+        this.tipoPantalla = tipoPantalla;
         this.tamanoMemoriaNAND = tamanoMemoriaNAND;
         this.sistemaOperativo = sistemaOperativo;
     }
-
 }
-
